8b4440d0c3d64a7cd76d2e77a611be8075f70079
ruesh dulal <rupesh.dulal@citytech.gloabl>
2023-08-09 12:57:40 +0545

    fix: kafka consumer producer

--- src/main/java/com/rupesh/MainVerticle.java
+++ src/main/java/com/rupesh/MainVerticle.java
@@ -2,25 +2,19 @@ package com.rupesh;
 
 import com.rupesh.configuration.CassandraClientProvider;
 import com.rupesh.configuration.CustomRetrieverConfiguration;
-import com.rupesh.factory.InstanceFactory;
-import com.rupesh.handler.user.list.AllUserHandler;
-import com.rupesh.kafka.EventProducer;
-import com.rupesh.kafka.IEventProducer;
-import com.rupesh.kafka.KafkaConnectionProvider;
-import com.rupesh.mqtt.MqttClientConfiguration;
-import com.rupesh.mqtt.MqttServerConfiguration;
 import com.rupesh.verticle.UserRestServer;
 import io.vertx.cassandra.CassandraClient;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.impl.logging.Logger;
 import io.vertx.core.impl.logging.LoggerFactory;
-import io.vertx.core.json.JsonObject;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
 import io.vertx.kafka.client.producer.KafkaProducer;
 import io.vertx.kafka.client.producer.KafkaProducerRecord;
-import io.vertx.kafka.client.serialization.JsonObjectSerializer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.HashMap;
@@ -36,15 +30,6 @@ public class MainVerticle extends AbstractVerticle {
     final CassandraClient client = CassandraClientProvider.getConnection(vertx);
 
     CustomRetrieverConfiguration.init(vertx).onSuccess((success) -> {
-
-      new EventProducer(KafkaConnectionProvider.getConnection(vertx, JsonObjectSerializer.class))
-        .eventPublish(new JsonObject().put("message", "THIS IS TEST MESSAGE"))
-        .onSuccess(eventSuccess -> {
-          System.out.println("ON EVENT SUCCESS : " + eventSuccess);
-        })
-        .onFailure(error -> {
-          System.out.println(error.getMessage());
-        });
 
       CassandraClientProvider
         .pingCassandraConnection(client)
@@ -64,26 +49,10 @@ public class MainVerticle extends AbstractVerticle {
 
   }
 
-//  public static void main(String[] args) {
-//    Vertx vertx = Vertx.vertx();
-//    vertx.deployVerticle(new MainVerticle());
-//  }
-
   public static void main(String[] args) {
     Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new MainVerticle());
 
-    Map<String, String> configMap = new HashMap<>();
-    configMap.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-    configMap.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-    configMap.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-
-    KafkaProducer<String, String> producer = KafkaProducer.create(vertx, configMap);
-
-    for (int i = 0; i < 5; i++) {
-      producer.write(KafkaProducerRecord.create("test", "message_" + i));
-    }
-    vertx.close();
-    producer.close();
   }
 
 }
--- src/main/java/com/rupesh/kafka/IEventProducer.java
+++
@@ -1,9 +0,0 @@
-package com.rupesh.kafka;
-
-import io.vertx.core.Future;
-import io.vertx.core.json.JsonObject;
-
-public interface IEventProducer {
-  Future<String> eventPublish(final JsonObject message);
-
-}
